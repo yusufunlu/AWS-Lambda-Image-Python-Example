6ea5bf2af1ef5eaa34fa57c23dc6e627203c6941
yusufubitlo <unlu.yufus@gmail.com>
2021-08-22 21:39:42 +0300

    exception handling and response model updates on code
    test cases detailed, s3 bucket naming disabled for now

--- decimal_to_roman/decimal_to_roman/app.py
+++ decimal_to_roman/decimal_to_roman/app.py
@@ -1,20 +1,42 @@
 import json
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    print("input body: %s" % (event.get("body")))
-    decimalNumber = int(event.get("body"))
-    print("decimalNumber: %d" % (decimalNumber))
+    logger.info("Request: %s", event)
+    body = event.get('body')
 
+    response_code = 200
+    responseBody = ""
 
-    decimalArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    romanSymbolArray = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-    roman_num = ''
+    if body is not None:
+        try:
+            jsonBody = json.loads(body)
+            decimalValue = jsonBody.get('decimal')
+            decimalNumber = int(decimalValue)
+        except Exception as e:
+            logger.exception("Exception occured: %s", e.__class__)
+            response_code = 400
+            responseBody = json.dumps({'message': str(e), 'rawRequest': event})
+        else:
+            decimalArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+            romanSymbolArray = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+            roman_num = ''
 
-    i = 0
-    while  decimalNumber > 0:
-        for _ in range(decimalNumber // decimalArray[i]):
-            roman_num += romanSymbolArray[i]
-            decimalNumber -= decimalArray[i]
-        i += 1
-    return roman_num
+            i = 0
+            while  decimalNumber > 0:
+                for _ in range(decimalNumber // decimalArray[i]):
+                    roman_num += romanSymbolArray[i]
+                    decimalNumber -= decimalArray[i]
+                i += 1
+            responseBody = json.dumps({'roman': roman_num})
+
+    response = {
+        'statusCode': response_code,
+        'body': responseBody
+    }
+
+    logger.info("Response: %s", response)
+    return response
